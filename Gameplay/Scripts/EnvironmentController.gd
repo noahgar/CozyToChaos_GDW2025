fa90a1ed89814b967296cdf64723854886574ee4
Noah Garcia <111128198+noahgar@users.noreply.github.com>
2025-03-11 22:00:39 +0100

    ambience and lighting blends, assigned materials, removed old conctept art

---
+++ Gameplay/Scripts/EnvironmentController.gd
@@ -0,0 +1,50 @@
+extends Node3D
+
+@export_category("Sun")
+@export var directionalLight:DirectionalLight3D
+@export var sunColor:Gradient
+@export var rainySunColor:Gradient
+@export var shadowOpacity:Curve
+
+@export_category("Environment")
+@export var worldEnvironment:WorldEnvironment
+var envMat
+@export var skyColor:Gradient
+@export var horizonColor:Gradient
+@export var rainySkyColor:Gradient
+@export var rainyHorizonColor:Gradient
+
+@export_category("Weather")
+@export var rainVFX:GPUParticles3D
+@export_range(0, 1) var rain:float
+@export var windVFX:GPUParticles3D
+@export_range(0, 1) var wind:float
+#TimeOfDay sollte ein wert zwischen 0-1 sein. Für die Rotation der Sonne bin ich mir nicht sicher, vielleicht einfach gar nicht rotieren?
+#timeOfDay = 0 -> Nacht, timeOfDay = 0.5 -> Tag, timeOfDay = 1 -> Nacht
+var timeOfDay:float
+
+func _ready() -> void:
+	envMat = worldEnvironment.environment.sky.sky_material
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timeOfDay += delta * 0.03
+	timeOfDay = fmod(timeOfDay, 1.0)
+	
+	if directionalLight != null:
+		var currentSun = lerp(sunColor.sample(timeOfDay), rainySunColor.sample(timeOfDay), rain)
+		var currentShadow = shadowOpacity.sample(timeOfDay) * (1 - rain)
+		directionalLight.light_color = currentSun
+		directionalLight.shadow_opacity = currentShadow
+	
+	if envMat != null:
+		var currentSky = lerp(skyColor.sample(timeOfDay), rainySkyColor.sample(timeOfDay), rain)
+		var currentHorizon = lerp(horizonColor.sample(timeOfDay), rainyHorizonColor.sample(timeOfDay), rain)
+		envMat.set_shader_parameter("skyColor", currentSky)
+		envMat.set_shader_parameter("horizonColor", currentHorizon)
+	
+	if rainVFX != null:
+		rainVFX.amount_ratio = rain
+	
+	if windVFX != null:
+		windVFX.amount_ratio = wind
+	
