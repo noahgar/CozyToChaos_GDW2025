98a862a3ccf82a46e702ecd339801f121d90812b
Gordon <gordon-git@rivers.wine>
2025-03-13 11:14:20 +0100

    Sleep message

--- Gameplay/Scripts/Inventory.gd
+++ Gameplay/Scripts/Inventory.gd
@@ -25,7 +25,6 @@ func putObjectBack():
 	if CurrentlyHolding:
 		CurrentlyHolding.putMyselfBack()
 		var root:RootSceneScript = get_tree().root.get_child(0)
-		root.proceedToStep(1)
 	
 	CurrentlyHolding = null
 
